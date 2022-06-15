e239e9cf3be32a745e444658ac570df3e8af3d0a
samuel <samuel14sromovsky@gmail.com>
2022-06-15 17:42:56 +0200

    initial commit

---
+++ src/main/java/com/example/springboot_angular_project_backend/SpringbootAngularProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot_angular_project_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAngularProjectBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootAngularProjectBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springboot_angular_project_backend/SpringbootAngularProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot_angular_project_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAngularProjectBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
