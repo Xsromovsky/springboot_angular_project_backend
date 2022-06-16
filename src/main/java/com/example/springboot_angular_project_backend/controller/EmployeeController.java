7fbbc944d620ffafa48dd6a6fafa257d776536c9
samuel <samuel14sromovsky@gmail.com>
2022-06-16 11:24:21 +0200

    get employee by id, update employee by id

--- src/main/java/com/example/springboot_angular_project_backend/controller/EmployeeController.java
+++ src/main/java/com/example/springboot_angular_project_backend/controller/EmployeeController.java
@@ -1,13 +1,14 @@
 package com.example.springboot_angular_project_backend.controller;
 
 
+import com.example.springboot_angular_project_backend.exception.ResourceNotFoundException;
 import com.example.springboot_angular_project_backend.model.Employee;
 import com.example.springboot_angular_project_backend.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/v1/")
 public class EmployeeController {
@@ -20,5 +21,30 @@ public class EmployeeController {
     public Iterable<Employee> getAllEmployees() {
         return employeeRepository.findAll();
     }
+
+    // create employee REST API
+    @PostMapping("employees")
+    public Employee createEmployee(@RequestBody Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    // get employee by id REST API
+    @GetMapping("employees/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id) {
+        Employee employee = employeeRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Employee not found with id " + id));
+        return ResponseEntity.ok(employee);
+    }
+
+    // update employee REST API
+    @PutMapping("employees/{id}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable Long id, @RequestBody Employee employee) {
+        Employee employee1 = employeeRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Employee not found with id " + id));
+        employee1.setFirstName(employee.getFirstName());
+        employee1.setLastName(employee.getLastName());
+        employee1.setEmail(employee.getEmail());
+        Employee updatedEmployee = employeeRepository.save(employee1);
+        return ResponseEntity.ok(updatedEmployee);
+    }
+
 
 }
--- src/main/java/com/example/springboot_angular_project_backend/exception/ResourceNotFoundException.java
+++ src/main/java/com/example/springboot_angular_project_backend/exception/ResourceNotFoundException.java
@@ -13,5 +13,4 @@ public class ResourceNotFoundException extends RuntimeException {
 
 
 
-
 }
